77c4b39aadb5d7a74342a38661bcd6592cbd6835
YinAqu <gbackmail@gmail.com>
2021-07-21 23:14:43 +0800

    fcitx5 & wallpaper

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=16" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=16";
@@ -38,7 +38,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+//static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
 
 static const Rule rules[] = {
 	/* xprop(1):
---
+++ scripts/autostart.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/bin/bash ~/.dwm/wp-autochange.sh &
+picom -b
+fcitx5 -d &
